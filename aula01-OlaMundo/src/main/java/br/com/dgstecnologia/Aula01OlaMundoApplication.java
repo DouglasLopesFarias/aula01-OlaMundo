7d10e98c50dc23eb9226e51beef244ddb9812a11
Douglas <douglaslopesfarias@outlook.com>
2021-09-08 22:26:38 -0300

    Projetct aula01-OlaMundo created

---
+++ aula01-OlaMundo/src/main/java/br/com/dgstecnologia/Aula01OlaMundoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgstecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01OlaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula01OlaMundoApplication.class, args);
+	}
+
+}
---
+++ aula01-OlaMundo/src/test/java/br/com/dgstecnologia/Aula01OlaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dgstecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01OlaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
